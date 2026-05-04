3e07937ba1db5579f1064d0dbc73d7b53a2156ff
IllyesBenedek <benedekillyes@gmail.com>
2026-05-04 17:00:58 +0200

    html001-teljesen-kesz

--- Html/Html001/test_szoliman.py
+++ Html/Html001/test_szoliman.py
@@ -31,11 +31,10 @@ def test_05_bekezdesek_szama(html_soup):
     assert len(ps) == 3, f"HIBA: Pontosan 3 bekezdés kell, de {len(ps)} van!"
 
 def test_06_h2_alcimek(html_soup):
-    exp = {"Egy", "Kettő", "Három"}
-    act = {t.text.strip() for t in html_soup.find_all("h2")}
-    hiany = exp - act
-    
-    assert not hiany, f"HIBA: {len(hiany)} hiányzik ({', '.join(hiany)}). Megvan: {len(act)}/3"
+    expected = ["A szemrehányás", "A szentkönyv", "A leborulás"]
+    actual = [h.text.strip() for h in html_soup.find_all("h2")]
+    hianyzo = [item for item in expected if item not in actual]
+    assert len(actual) == 3, f"HIBA: 3 cím kellene, de csak {len(actual)} van! Hiányzik: {hianyzo}"
     
 def test_07_dolt_tekintete(html_soup):
     p1 = html_soup.find_all("p")[0]
